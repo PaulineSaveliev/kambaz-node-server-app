63a411b91513e529bb7ec3dfb23a207f8a503882
Pauline Saveliev <paulinesaveliev@yahoo.com>
2025-06-07 12:58:57 -0700

    account RESTful

---
+++ Kambaz/Database/courses.js
@@ -0,0 +1,79 @@
+export default [
+  {
+    _id: "RS101",
+    image: "RS101",
+    name: "Rocket Propulsion",
+    number: "RS4550",
+    startDate: "2023-01-10",
+    endDate: "2023-05-15",
+    department: "D123",
+    credits: 4,
+    description: "This course provides an in-depth study of the fundamentals of rocket propulsion, covering topics such as propulsion theory, engine types, fuel chemistry, and the practical applications of rocket technology. Designed for students with a strong background in physics and engineering, the course includes both theoretical instruction and hands-on laboratory work"
+  },
+  {
+    _id: "RS102",
+    image: "RS102",
+    name: "Aerodynamics",
+    number: "RS4560",
+    startDate: "2023-01-10",
+    endDate: "2023-05-15",
+    department: "D123",
+    credits: 3,
+    description: "This course offers a comprehensive exploration of aerodynamics, focusing on the principles and applications of airflow and its effects on flying objects. Topics include fluid dynamics, airfoil design, lift and drag forces, and the aerodynamic considerations in aircraft design. The course blends theoretical learning with practical applications, suitable for students pursuing a career in aeronautics or astronautics engineering."
+  },
+  {
+    _id: "RS103",
+    image: "RS103",
+    name: "Spacecraft Design",
+    number: "RS4570",
+    startDate: "2023-01-10",
+    endDate: "2023-05-15",
+    department: "D123",
+    credits: 4,
+    description: "This course delves into the principles and practices of spacecraft design, offering students a detailed understanding of the engineering and technology behind spacecraft systems. Key topics include spacecraft structure, propulsion, power systems, thermal control, and payload integration. Emphasizing both theoretical concepts and practical skills, the course prepares students for careers in the space industry, with a focus on innovative design and problem-solving in the context of current and future space missions"
+  },
+  {
+    _id: "RS104",
+    image: "RS104",
+    name: "Organic Chemistry",
+    number: "CH1230",
+    startDate: "2023-01-10",
+    endDate: "2023-05-15",
+    department: "D134",
+    credits: 3,
+    description: "Organic Chemistry is an in-depth course that explores the structure, properties, composition, and reactions of organic compounds and materials. The course covers various topics including hydrocarbons, functional groups, stereochemistry, and organic synthesis techniques. Students will learn about the mechanisms of organic reactions, spectroscopic methods for structure determination, and the role of organic chemistry in biological systems. Emphasis is placed on problem-solving and laboratory skills, preparing students for advanced studies in chemistry, medicine, and related fields."
+  },
+  {
+    _id: "RS105",
+    image: "RS105",
+    name: "Intro to Marvel Comics",
+    number: "CH1240",
+    startDate: "2023-01-10",
+    endDate: "2023-05-15",
+    department: "D134",
+    credits: 3,
+    description: "Introduction to Marvel Comics focuses on the impact of Marvel Comics on popular culture and examines recent events in the comics, with a particular focus on cosmic heroes."
+  },
+  {
+    _id: "RS106",
+    image: "RS106",
+    name: "Ancient Languages and Scripts of Middle-earth",
+    number: "ME101",
+    startDate: "2023-01-10",
+    endDate: "2023-05-15",
+    department: "Languages",
+    credits: 3,
+    description: "This course offers an exploration of the ancient languages and scripts found throughout Middle-earth, including Elvish (Sindarin and Quenya), Dwarvish (Khuzdul), and the Black Speech of Mordor. Students will learn the historical and cultural contexts of these languages, their linguistic structures, and their usage in various inscriptions and texts. Emphasis is on understanding the philological aspects and the role of language in shaping Middle-earth's history and lore."
+  },
+  {
+    _id: "RS107",
+    image: "RS107",
+    name: "Wizards, Elves, and Men: Inter-species Diplomacy in Middle-earth",
+    number: "ME102",
+    startDate: "2023-01-10",
+    endDate: "2023-05-15",
+    department: "Political Studies",
+    credits: 4,
+    description: "This course explores the complex relationships and diplomatic interactions among the different races of Middle-earth: Elves, Men, Dwarves, and Wizards. Topics include the study of historical alliances, conflicts, and the role of leadership and wisdom in maintaining peace. Students will engage in discussions and case studies on key events in Middle-earth's history, such as the Council of Elrond and the War of the Ring, to understand the principles of diplomacy and conflict resolution in a multi-species context."
+  }
+]
---
+++ Kambaz/Database/index.js
@@ -0,0 +1,10 @@
+// Pauline Saveliev
+// CS5610 - Summer 1 2025
+
+import courses from "./courses.js"
+import modules from "./modules.js"
+import assignments from "./assignments.js"
+import users from "./users.js"
+import enrollments from "./enrollments.js"
+
+export default { courses, modules, assignments, users, enrollments };
